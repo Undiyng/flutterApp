39b29304b7162142e0e7015d8bb913e48ea14881
Eduardo Ramirez <eduard.jrl21@gmail.com>
2025-11-07 21:03:33 -0400

    vercel fixXSkkjss

--- components/routes/networkPromotionGet.js
+++ components/routes/networkPromotionGet.js
@@ -3,8 +3,8 @@ const Router = express.Router();
 const promotionController = require("../controllers/promotionController");
 const response = require("../response/response");
 
-Router.get("/", ()=>{
-return "hola mundo"
+Router.get("/", (req,res)=>{
+res.send("hola mundo")
 }
 );
 
